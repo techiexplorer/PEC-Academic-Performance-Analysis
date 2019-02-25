7440b6b14a9f3892f820b5f08b49437ad980d6c1
Iftekharuddin Mohammed <farhan_mohammed98@outlook.com>
2019-02-26 02:25:28 +0530

    Update pec_analytic_software_dependencies.bat

--- dependencies/pec_analytic_software_dependencies.bat
+++ dependencies/pec_analytic_software_dependencies.bat
@@ -22,12 +22,19 @@ echo 1
 ping -n 2 127.0.0.1>nul
 
 echo Let's begin !
+echo Installing Python 3.6.5 Silently
 python-3.6.5-amd64.exe /quiet PrependPath=1 Include_launcher=0
+echo Installing pip
 python get-pip.py
+echo Installing pandas
 pip install pandas-0.24.1-cp36-cp36m-win_amd64.whl
+echo Installing numpy
 pip install numpy-1.16.1-cp36-cp36m-win_amd64.whl
+echo Installing matplotlib
 pip install matplotlib-3.0.2-cp36-cp36m-win_amd64.whl
+echo Installing Pillow-PIL
 pip install Pillow-5.4.1-cp36-cp36m-win_amd64.whl
+echo Installing xlrd
 pip install xlrd-1.2.0-py2.py3-none-any.whl
 
 
